bc7da2319feae27427457cb30c5a147ce72af526
shrvan <shrvan.abosh@yahoo.com>
2020-05-01 17:24:09 +0300

    test

---
+++ module/js/app.js
@@ -0,0 +1,21 @@
+import { Superman as AnotherSuperman } from'./supermnan.js';
+import { Mage } from'./mage.js';
+
+class Superman {
+
+}
+var clark = new Superman("Clark Kent", 1000, 1000);
+
+var xevras = new Mage("Xevras", 100, 300);
+
+clark.firePrimary(xevras);
+xevras.firePrimary(clark);   
+xevras.fireSecondary(clark);
+clark.fireSecondary(xevras);
+
+
+console.log(clark);
+console.log(xevras);
+
+console.log(clark.isAlive);
+console.log(xevras.isAlive);
